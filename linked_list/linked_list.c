2fc50d455e039d6358e8544070b41468b58e6538
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-10 20:28:07 +0530

    implemented linked list to insert element at the beginning

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -15,7 +15,7 @@ void Print();
 int main(void)
 {
     head = NULL;
-    printf("Hoe many numbers?\n");
+    printf("How many numbers?\n");
     int n, i, x;
     scanf("%d", &n);
     for (i = 0; i < n; i++)
@@ -34,6 +34,18 @@ void Insert(int x)
 {
     struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
     temp -> data = x;
-    temp -> next = NULL;
+    temp -> next = head;
     head = temp;
+}
+
+void Print() 
+{
+    struct Node* temp = head;
+    printf("List is: ");
+    while (temp != NULL)
+    {
+        printf(" %d", temp -> data);
+        temp = temp -> next;
+    }
+    printf("\n");
 }
