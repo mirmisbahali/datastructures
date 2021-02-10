2f0aadbdcbb36e700e4bd0e932af93090a4efb00
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-10 20:24:40 +0530

    writing Insert function

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,12 +1,39 @@
-#include <stdio.h>
+// Inserting a node at beginning
 
-struct node
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
 {
     int data;
-    struct node* link;
+    struct Node* next;
 };
+struct Node* head;
+
+void Insert(int x);
+void Print();
 int main(void)
 {
+    head = NULL;
+    printf("Hoe many numbers?\n");
+    int n, i, x;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the number \n");
+        scanf("%d", &x);
+        Insert(x);
+        Print();
+    }
+
 
     return 0;
+}
+
+void Insert(int x)
+{
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp -> data = x;
+    temp -> next = NULL;
+    head = temp;
 }
