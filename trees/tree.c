f36c31f9ad5ade7f95cc4130fb5db66d264c6085
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-04-11 22:26:08 +0530

    searching in binary search tree

--- trees/tree.c
+++ trees/tree.c
@@ -16,18 +16,40 @@ struct node* getNewNode(int x)
     return newNode;
 }
 
-struct node* inesrt(struct node *root, int x)
+struct node* insert(struct node *root, int x)
 {
     if (root == NULL) 
-    {
-        root = getNewNode(data);
-        return root;
-    }
+        root = getNewNode(x);
+    else if (x <= root->data)
+        root->left = insert(root->left, x);
+    else
+        root->right = insert(root->right, x);
+    return root;
+}
+
+int search(struct node *root, int x)
+{
+    if (root == NULL) return 0;
+    else if (root->data == x) return 1;
+    else if (x <= root->data) return search(root->left, x);
+    else return search(root->right, x);
 }
 
 int main(void)
 {
     struct node *root = NULL;
+    int number;
     root = insert(root, 5);
+    root = insert(root, 20);
+    root = insert(root, 3);
+    root = insert(root, 40);
+
+    printf("Search a number: ");
+    scanf("%d", &number);
+    if (search(root, number))
+        printf("Found\n");
+    else
+        printf("Not Found\n");
+     
     return 0;
 }
