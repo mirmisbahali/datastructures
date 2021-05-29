5c7582432586b9acb8fb3eb5675e091e2cc95fc7
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-05-29 07:38:52 +0530

    sorting 0, 1, 2

--- array/0_1_2.c
+++ array/0_1_2.c
@@ -24,27 +24,34 @@ int main(void)
     // sort1(arr, n);
 
     /* Method 2
-    count number of 0s, 1s, and 2s,
-    Then add them in the array in assending order
+    Algorithm: 
+
+    1. Keep three indices low = 1, mid = 1 and high = N and there are four ranges, 1 to low (the range containing 0), low to mid (the range containing 1), mid to high (the range containing unknown elements) and high to N (the range containing 2).
+    2. Traverse the array from start to end and mid is less than high. (Loop counter is i)
+    3. If the element is 0 then swap the element with the element at index low and update low = low + 1 and mid = mid + 1
+    4. If the element is 1 then update mid = mid + 1
+    5. If the element is 2 then swap the element with the element at index high and update high = high – 1 and update i = i – 1. As the swapped element is not processed
+    6. Print the output array.
     */
-    int count0=0, count1=0, count2=0;
-    for (int i = 0; i < n; i++)
+    int low=0, mid=0, high=n, temp=0;
+    for (int i = 0; i < n, mid < high; i++)
     {
-        if (arr[i] == 0) count0++;
-        else if (arr[i] == 1) count1++;
-        else count2++;
-    }
-    for (int i = 0; i < count0; i++)
-    {
-        arr[i] = 0;
-    }
-    for (int i = count0, j = 0; j < count1-1; i++, j++)
-    {
-        arr[i] = 1;
-    }
-    for (int i = count1, k = 0; k < count2-1; i++, k++)
-    {
-        arr[i] = 2;
+        if (arr[i] == 0)
+        {
+            temp = arr[i];
+            arr[i] = arr[low];
+            arr[low] = temp;
+            low++; 
+        }
+        else if (arr[i] == 2)
+        {
+            temp = arr[i];
+            arr[i] = arr[high];
+            arr[high] = temp;
+            high--;
+        }
+        else mid++;
+
     }
 
     for (int i = 0; i < n; i++)
