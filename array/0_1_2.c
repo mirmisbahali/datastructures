1af02b9aaf4b95fc1cbbac09077e61089c7b419b
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-05-29 07:45:02 +0530

    method 2 to sort 0,1,2

--- array/0_1_2.c
+++ array/0_1_2.c
@@ -23,7 +23,7 @@ int main(void)
 
     // sort1(arr, n);
 
-    /* Method 2
+/* Method 2
     Algorithm: 
 
     1. Keep three indices low = 1, mid = 1 and high = N and there are four ranges, 1 to low (the range containing 0), low to mid (the range containing 1), mid to high (the range containing unknown elements) and high to N (the range containing 2).
@@ -32,7 +32,7 @@ int main(void)
     4. If the element is 1 then update mid = mid + 1
     5. If the element is 2 then swap the element with the element at index high and update high = high – 1 and update i = i – 1. As the swapped element is not processed
     6. Print the output array.
-    */
+
     int low=0, mid=0, high=n, temp=0;
     for (int i = 0; i < n, mid < high; i++)
     {
@@ -53,6 +53,26 @@ int main(void)
         else mid++;
 
     }
+*/
+
+/*
+    Method 3
+    count all 0s, 1s, and 2s.
+    add these number of 0s, 1s and 2s in array
+*/
+    int count0=0, count1=0, count2=0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0) count0++;
+        else if (arr[i] == 1) count1++;
+        else count2++;
+    }
+
+    for (int i = 0; i < count0; i++) arr[i] = 0;
+    for (int i = count0; i < (count0 + count1); i++) arr[i] = 1;
+    for (int i = (count0 + count1); i < n; i++) arr[i] = 2;
+
 
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
