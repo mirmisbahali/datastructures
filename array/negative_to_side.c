26b55d8c7f0f2b0ce06859f5c55469db06099daf
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-05-31 09:38:34 +0530

    solved negative to side

--- array/negative_to_side.c
+++ array/negative_to_side.c
@@ -1,31 +1,29 @@
 // Move all negative numbers to beginning and positive to end with constant extra space
 #include<stdio.h>
 
+void Partition(int *arr, int n)
+{
+    int pivot = 0, pindex = 0, temp=0;
+    
+    for (int i = 0; i < n; i++)
+    {
+
+        if (*(arr + i) < pivot)
+        {
+            temp = *(arr + pindex);
+            *(arr + pindex) = *(arr + i);
+            *(arr + i) = temp;
+            pindex++;
+        }
+    }
+}
+
 int main(void)
 {
     int arr[] = {-12, 11, -13, -5, -6, -7, 5, -3, -6}, n = 9;
     
-    int low=0, high=n , temp=0;
+    Partition(arr, n);
     
-    int i = 0;
-    while (low < high && i < n)
-    {
-        if (arr[i] < 0)
-        {
-            temp = arr[i];
-            arr[i] = arr[low];
-            arr[low] = temp;
-            low++; 
-        }
-        if (arr[i] > 0)
-        {
-            temp = arr[i];
-            arr[i] = arr[high];
-            arr[high] = temp;
-            high--;
-        }
-        i++;
-    }
 
     for (int i = 0; i < n; i++)
     {
